4ba3bb467765b6ef78ea70ce1d2492c10ed0e8fe
asp <asp@LAB3-104>
2023-06-21 11:04:47 +0500

    Add project files.

---
+++ Crud/Controllers/DepartmentController.cs
@@ -0,0 +1,70 @@
+﻿using Crud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crud.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        crud_with_angular_ajaxEntities db = new crud_with_angular_ajaxEntities();
+
+        public ActionResult Index()
+        {
+            List<tbl_department> deplist = db.tbl_department.ToList();
+            ViewBag.dep_list = new SelectList(deplist, "DepartmentId", "DepartmentName");
+            return View();
+        }
+
+        public JsonResult GetStudentList()
+        {
+            List<StudentViewModel> Stulist = db.tbl_Student
+                .Where(x => x.isDeleted == false)
+                .Select(x => new StudentViewModel
+                {
+                    Studentld = x.StudentId,
+                    StudentName = x.StudentName,
+                    StudentEmail = x.StudentEmail,
+                    DepartmentName = x.tbl_department.DepartmentName
+                })
+                .ToList();
+
+            return Json(Stulist, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult SaveDataInDatabase(StudentViewModel model)
+        {
+            var result = false;
+            try
+            {
+                if (model.Studentld > 0)
+                {
+                    tbl_Student Stu = db.tbl_Student.SingleOrDefault(x => x.isDeleted == false && x.StudentId == model.Studentld);
+                    Stu.StudentName = model.StudentName;
+                    Stu.StudentEmail = model.StudentEmail;
+                    Stu.DepartmentId = model.DepartmentId; 
+                    db.SaveChanges();
+                }
+                else
+                {
+                    result = true;
+                    tbl_Student Stu = new tbl_Student(); 
+                    Stu.StudentName = model.StudentName; 
+                    Stu.StudentEmail = model.StudentEmail;
+                    Stu.DepartmentId = model.DepartmentId; 
+                    Stu.isDeleted = false;
+                    db.tbl_Student.Add(Stu);
+                    db.SaveChanges();
+                    result = true;
+                }
+            }
+            catch ( Exception ex)
+            {
+                throw;
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Crud/Models/StudentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Crud.Models
+{
+    public class StudentViewModel
+    {
+        public int Studentld { get; set; }
+        public string StudentName { get; set; }
+        public string StudentEmail { get; set; }
+        public Nullable<bool> isDeleted { get; set; }
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
